cabc2188bf53ca4f20cfdb7ab1e82e082f92a564
Rostyslav <rostyslav.skrypnyk@globallogic.com>
2019-07-18 00:26:30 +0300

    Create Input

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function Input(props) {
+    return (
+        <div>
+            <input className={props.error ? "red" : ""} value={props.value} type={props.type} onChange={props.onChange} />
+        </div>
+    )
+}
+
+Input.propTypes = {
+    value: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(['password', 'button', 'image']).isRequired,
+    error: PropTypes.bool.isRequired,
+    onChange: PropTypes.func.isRequired
+}
+
+export default Input;
--- src/pages/Uikit.jsx
+++ src/pages/Uikit.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Wrapper from '../components/Wrapper/Wrapper';
 import Button from '../components/Button'
+import Input from '../components/Input/Input'
 
 function Uikit() {
+
+   const handleChange=(e)=> {
+        this.setState({value: e.target.value});
+      }
     const click = () => {
         console.log('click method');
     }
@@ -11,6 +16,7 @@ function Uikit() {
             <Button color="red" onClick={click} text='red button' />
             <Button color="green" onClick={click} text='green button' />
             <Button color="yellow" link="google.com" text='link' />
+            <Input error="false" value="some value" type={props.type} onChange={props.onChange}/>
         </Wrapper>
     )
 }
